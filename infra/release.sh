dad7d2e592a6333e5ab2521b45c00c17fe516767
Jose Ramon Mañes <jr.mb47@gmail.com>
2022-01-04 00:12:53 +0100

    fix scripte

--- infra/release.sh
+++ infra/release.sh
@@ -7,11 +7,11 @@ PROJECT_NAME=piktoctl
 mkdir -p ./artifacts
 for a in ${goos[@]}; do
 	for i in ${arch[@]}; do
-                if [[ "$a" == "darwin" && "$i" == "386" ]] || [[ "$a" == "windows" && "$i" == "arm64" ]];then
+    if [[ "$a" == "darwin" && "$i" == "386" ]] || [[ "$a" == "windows" && "$i" == "arm64" ]];then
 	  		echo "Not allowed: $a $i"
 		else
 	  		echo "Building: $a $i"
- 			CGO_ENABLED=0 GOOS=$a GOARCH=$i go build -o ./bin/$a/$i/${PROJECT_NAME}./ &&\
+ 			CGO_ENABLED=0 GOOS=$a GOARCH=$i go build -o ./bin/$a/$i/${PROJECT_NAME} &&\
 			tar -czvf ./artifacts/${PROJECT_NAME}-$a-$i.tar.gz ./bin/$a/$i/${PROJECT_NAME} &
 		fi
 	done
